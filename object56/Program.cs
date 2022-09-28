69c722fca0e04a3fe2b5f1748820245e29ee4213
Nikolay Lebedinskiy <nikolaylebedinskiy1983.ru>
2022-09-28 19:20:14 +0300

    Задача 56

---
+++ object56/Program.cs
@@ -0,0 +1,50 @@
+﻿int[,] GenerateArray(int rows, int columns, int minrnd, int maxrnd)
+{
+    int[,] array = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(minrnd, maxrnd + 1);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write($"{matrix[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+    System.Console.WriteLine();
+}
+
+int LineMinSum(int[,] array)
+{
+    int minSum = array[0, 0];
+    int lineMin = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum += array[i, j];
+        }
+        if (minSum > sum)
+            {
+                minSum = sum;
+                lineMin = i;
+            }
+    }
+    return lineMin + 1;
+}
+
+int[,] myarray = GenerateArray(6, 3, 0, 10);
+PrintArray(myarray);
+System.Console.WriteLine($"Строка с минимальной суммой:  {LineMinSum(myarray)}");
